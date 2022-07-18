99e29bcbca51a5938818287dbd673d2cc6878deb
Mamad <dehghanseyedmohammad@gmail.com>
2022-07-19 01:02:22 +0430

    fix(tailwind.config): add custom colors to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary-red':'#FF626D',
+        'primary-orange':'#FCAD72',
+        'weef-black':'#232227',
+        'secondary':'#333235',
+        'weef-white':'#FAFAFA',
+        'weef-secondary-light':'#4C4C4C',
+        'weef-grey':'#C2C2C2'
+      }
+    },
   },
   plugins: [],
 }
