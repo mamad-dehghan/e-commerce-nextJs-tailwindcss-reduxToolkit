f9516d1839a8786492b4d191103607c1ce66a825
Mamad <dehghanseyedmohammad@gmail.com>
2022-07-23 21:22:18 +0430

    feat(layout): create site default layout

---
+++ layouts/DefaultLayout/index.tsx
@@ -0,0 +1,21 @@
+import React, {ReactElement} from 'react';
+import Header from "../Header";
+import Footer from "../Footer";
+
+type props = {
+    children: ReactElement ;
+}
+
+const DefaultLayout = ({children}: props) => {
+    return (
+        <div className='w-full min-h-screen flex flex-col gap-4'>
+            <Header/>
+            <>
+                {children}
+            </>
+            <Footer/>
+        </div>
+    );
+}
+
+export default DefaultLayout;
