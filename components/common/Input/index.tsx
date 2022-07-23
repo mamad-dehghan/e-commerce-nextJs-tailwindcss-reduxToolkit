de2fb71f394c21f097713599f02e11a03b01fb07
Mamad <dehghanseyedmohammad@gmail.com>
2022-07-23 20:56:57 +0430

    feat(input): add input component

---
+++ components/common/Input/index.tsx
@@ -0,0 +1,13 @@
+import React, {InputHTMLAttributes, useEffect, useState} from 'react';
+
+const Input = ({...props}: InputHTMLAttributes<HTMLInputElement>) => {
+    
+    return (
+        <input
+            dir='auto'
+            className="bg-secondary  text-weef-white ring-[1px] ring-secondary placeholder-weef-grey outline-0 focus:outline-0  text-base rounded-md focus:border-0 border-0  focus:ring-primary-red block w-[320px] p-2.5 "
+            {...props}/>
+    );
+}
+
+export default Input;
