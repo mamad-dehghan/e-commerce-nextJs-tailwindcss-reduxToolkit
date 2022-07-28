e766b5ba60cac19021d9b38a19ffd9ced528e429
Mamad <dehghanseyedmohammad@gmail.com>
2022-07-28 17:39:13 +0430

    feat(carousel): add main page carousel for banners and introduce new products

---
+++ components/costum/Carousel/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, } from "swiper";
+
+const Carousel=()=> {
+    return (
+        <div id='carousel' className='max-w-[90rem] mx-auto'>
+            <Swiper
+                allowTouchMove={false}
+                modules={[Autoplay]}
+                loop={true}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                }}
+                slidesPerView={2}
+                style={{
+                    width:"100%",
+                    // maxWidth:"90rem",
+                    // background:"limegreen",
+                    padding:"7% 0"
+                }}
+            >
+                <SwiperSlide className='w-[50vw] max-w-[45rem] max-h-[27rem] h-[30vw] px-0' ><div className='banner w-full max-h-[27rem] h-[30vw] bg-amber-100' /></SwiperSlide>
+                <SwiperSlide className='w-[50vw] max-w-[45rem] max-h-[27rem] h-[30vw] px-0' ><div className='banner w-full max-h-[27rem] h-[30vw] bg-amber-200' /></SwiperSlide>
+                <SwiperSlide className='w-[50vw] max-w-[45rem] max-h-[27rem] h-[30vw] px-0' ><div className='banner w-full max-h-[27rem] h-[30vw] bg-amber-300' /></SwiperSlide>
+                <SwiperSlide className='w-[50vw] max-w-[45rem] max-h-[27rem] h-[30vw] px-0' ><div className='banner w-full max-h-[27rem] h-[30vw] bg-amber-400' /></SwiperSlide>
+                <SwiperSlide className='w-[50vw] max-w-[45rem] max-h-[27rem] h-[30vw] px-0' ><div className='banner w-full max-h-[27rem] h-[30vw] bg-amber-500' /></SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+}
+
+export default Carousel;
