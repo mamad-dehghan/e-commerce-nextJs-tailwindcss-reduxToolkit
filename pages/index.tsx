4b4984550f824c4065aca0c8431f818383cc4883
Mamad <dehghanseyedmohammad@gmail.com>
2022-07-31 21:07:32 +0430

    fix(homepage): replace static data with database data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,79 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import Header from "../layouts/Header";
-import Footer from "../layouts/Footer";
-import Accept from "../utilities/icons/Accept";
-import Reject from "../utilities/icons/Reject";
 import DefaultLayout from "../layouts/DefaultLayout";
-import ProductCard from "../components/common/ProductCard";
-import {useState} from "react";
-import image from './../public/e2.jpg'
-import CardSlider from "../components/costum/CardSlider";
-import SwiperButton from "../utilities/icons/SwiperButton";
 import Carousel from '../components/costum/Carousel';
 import Section from "../components/costum/Section";
-import CardsWrapper from "../components/costum/CardsWrapper";
+import axios from "axios";
+import IProduct from "../interfaces/IProduct";
+import ICategory from "../interfaces/ICategory";
 
-//
-// const convertBase64 = (file:any) => {
-//     return new Promise((resolve, reject) => {
-//         const fileReader = new FileReader();
-//         fileReader.readAsDataURL(file);
-//
-//         fileReader.onload = () => {
-//             resolve(fileReader.result);
-//         };
-//
-//         fileReader.onerror = (error) => {
-//             reject(error);
-//         };
-//     });
-// };
-
-// <ProductCard title={'کیف دستی'} price={'245.000'} image={image} colors={['#345487', '#A53f32', '#950bb1']} sizes={[40, 41, 42]} />
-const product = {
-    id:'adafe',
-    title: 'کیف دستی',
-    price: '245.000',
-    image: image,
-    colors: ['#345487', '#A53f32', '#950bb1'],
-    sizes: [40, 41, 42]
+type packageCategoryProducts = {
+    category: ICategory, products: IProduct[]
 }
-const products = [{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product}]
 
+type props = {
+    featured: IProduct[],
+    categories: packageCategoryProducts[]
+}
 
-const Home: NextPage = () => {
-    // const [file, setFile] = useState<any | null>(null)
-    //
-    // console.log(file)
-    //
-    // const change = async ()=>{
-    //     const base64 = await convertBase64(file);
-    //     console.log(base64)
-    // }
-
-
+// @ts-ignore
+const Home: NextPage = ({featured, categories}: props) => {
     return (
         <>
             <Head>
                 <title>WEEF Online Shop</title>
-                <link rel="icon" href="/favicon.ico"/>
             </Head>
             <DefaultLayout>
-                    <main className='bg-secondary'>
-                        <Carousel />
+                <main className='bg-secondary'>
+                    <Carousel/>
 
-                        <Section background='primary' title='کیف‌های مجلسی' >
-                            <CardsWrapper products={[{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product}]} />
-                        </Section>
+                    {
+                        categories.map(category => (
+                            <Section
+                                key={category.category.id}
+                                wrapper={{
+                                    products: category.products,
+                                    category: category.category
+                                }}
+                            />
+                        ))
+                    }
 
-                        <Section background='primary' title='کفش‌های ورزشی' >
-                            <CardsWrapper products={[{...product},{...product},{...product},{...product},{...product},{...product},{...product},{...product}]} />
-                        </Section>
+                    <Section
+                        slider={{
+                            title: 'محصولات پرفروش',
+                            products: featured
+                        }}
+                    />
 
-                        <Section background='black' title='محصولات پرفروش' >
-                            <CardSlider products={products}/>
-                        </Section>
-                    </main>
+                </main>
             </DefaultLayout>
         </>
     )
 }
 
 export default Home
+
+export async function getStaticProps() {
+    let fetch: IProduct[] = await axios('http://localhost:8000/store/product/featured?format=json')
+        .then((res: any) => res.data)
+
+    let fetchCategories: ICategory[] = await axios('http://localhost:8000/store/category/featured')
+        .then((res: any) => res.data)
+
+    const data: packageCategoryProducts[] = [];
+
+    for (const category of fetchCategories) {
+        const response: IProduct[] = await axios(`http://localhost:8000/store/product/category/slug/${category.slug}`)
+            .then(res => res.data);
+        data.push({category, products: response});
+    }
+
+    return ({
+        props: {
+            featured: fetch,
+            categories: data
+        }
+    })
+
+}
