d4000330480506360e5c2d65dac9994ccfa8af41
Mamad <dehghanseyedmohammad@gmail.com>
2022-08-11 00:47:05 +0430

    fix(single product page): fix when single product page reload with new product data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,22 @@ import type {AppProps} from 'next/app'
 import {Provider} from "react-redux";
 import {persist, store} from "../redux/store";
 import {PersistGate} from "redux-persist/integration/react";
+import {useRouter} from 'next/router';
+import {useMemo} from "react";
 
 function MyApp({Component, pageProps}: AppProps) {
+    const router = useRouter()
+    const key = useMemo(() => {
+        if (router.query.SubCategory)
+            return router.query.SubCategory
+
+        return router.asPath
+    }, [router.asPath])
+
     return (
         <Provider store={store}>
             <PersistGate persistor={persist}>
-                <Component {...pageProps} />
+                <Component key={key} {...pageProps} />
             </PersistGate>
         </Provider>
     )
