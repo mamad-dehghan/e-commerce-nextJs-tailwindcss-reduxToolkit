70b94804f35357dcc6c547f8883b638a7350c7a0
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 13:28:27 +0430

    fix(card slider): fix component style on phone size

--- components/costum/CardSlider/index.tsx
+++ components/costum/CardSlider/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import  {Navigation} from 'swiper'
+import {Navigation} from 'swiper'
 import ProductCard from "../../common/ProductCard";
 import SwiperButton from "../../../utilities/icons/SwiperButton";
 import 'swiper/css/navigation';
@@ -11,54 +11,51 @@ type props = {
     products: IProduct[]
 }
 
-const CardSlider=({products}: props)=> {
-
+const CardSlider = ({products}: props) => {
 
     return (
-        <>
-            <div className='Container cardSlider'>
-                <div className='w-full overflow-hidden lg:px-6  py-1'>
-                    <Swiper
-                        className='sm:w-[600px] md:w-[680px] lg:w-[936px] xl:w-[1200px]  mx-auto   '
-                        navigation={{
-                            prevEl: '.prev',
-                            nextEl: '.next',
-                        }}
-                        initialSlide={1}
-                        modules={[Navigation]}
-                        spaceBetween={8}
-                        slidesPerView={1}
-                        breakpoints={{
-                            640:{
-                                slidesPerView:2,
-                                initialSlide:2
-                            },
-                            1024: {
-                                slidesPerView: 3,
-                                initialSlide: 3
-                            },
-                            1280: {
-                                slidesPerView: 4,
-                                initialSlide: 4
-                            }
-                        }}
-                    >
-                        {products.map(product => (
-                            <SwiperSlide key={product.id}>
-                                <ProductCard product={product}/>
-                            </SwiperSlide>
-                        ))}
-                        <span className='prev absolute -translate-y-1/2 right-1 top-1/2 z-20 '
-                              slot='container-start'>
+        <div className='Container cardSlider'>
+            <div className='w-full overflow-hidden lg:px-6  py-1'>
+                <Swiper
+                    className='mx-auto'
+                    navigation={{
+                        prevEl: '.prev',
+                        nextEl: '.next',
+                    }}
+                    initialSlide={1}
+                    modules={[Navigation]}
+                    spaceBetween={8}
+                    slidesPerView={1}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            initialSlide: 2
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            initialSlide: 3
+                        },
+                        1280: {
+                            slidesPerView: 4,
+                            initialSlide: 4
+                        }
+                    }}
+                >
+                    {products.map(product => (
+                        <SwiperSlide key={product.id}>
+                            <ProductCard product={product}/>
+                        </SwiperSlide>
+                    ))}
+                    <span className='prev absolute -translate-y-1/2 right-1 top-1/2 z-20 '
+                          slot='container-start'>
                             <SwiperButton direction='right'/>
                         </span>
-                        <span className='next absolute -translate-y-1/2 top-1/2 z-20 left-1' slot='container-start'>
+                    <span className='next absolute -translate-y-1/2 top-1/2 z-20 left-1' slot='container-start'>
                             <SwiperButton direction='left'/>
                         </span>
-                    </Swiper>
-                </div>
+                </Swiper>
             </div>
-        </>
+        </div>
     );
 }
 
