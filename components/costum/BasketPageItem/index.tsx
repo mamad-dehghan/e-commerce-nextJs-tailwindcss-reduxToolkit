89fa62610fe35bb14dfe7ac16b6b80d59e0de41b
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 13:57:28 +0430

    fix(refactor): refactor component with rotated button and item row component

--- components/costum/BasketPageItem/index.tsx
+++ components/costum/BasketPageItem/index.tsx
@@ -8,6 +8,9 @@ import {addProduct, removeAllSingleProduct, removeProduct} from '../../../redux/
 import MultiButton from "../../common/MultiButton";
 import PaletteItem from "../../common/ProductCard/PaletteItem";
 import SizeItem from "../../common/ProductCard/SizeItem";
+import ItemRow from "../ItemRow";
+import {Colors} from "../../../utilities/constants/colors";
+import RotatedButton from "../../common/RotatedButton";
 
 type props = {
     product: IProduct,
@@ -20,13 +23,6 @@ const BasketPageItem = ({product, count, color = undefined, size = undefined}: p
     const [hover, setHover] = useState<boolean>(false);
     const dispatch = useDispatch()
 
-    const containerClassName = useMemo(() => {
-        return classNames(
-            'h-[96px] max-w-[980px] w-full p-[1px] bg-weef-secondary-light hover:bg-primary overflow-visible relative',
-            hover ? 'z-10' : ''
-        )
-    }, [hover])
-
     const wrapperClassName = useMemo(() => {
         return classNames(
             'w-full h-full flex justify-between items-center transition-all duration-75 pr-20 pl-14',
@@ -36,7 +32,7 @@ const BasketPageItem = ({product, count, color = undefined, size = undefined}: p
 
     const imageWrapper = useMemo(() => {
         return classNames(
-            'absolute top-0 right-0  translate-y-[calc(100%-105px)] w-[105px] h-[105px] rotate-45 group bg-primary-orange p-[1px] flex items-center justify-center transition-all duration-300',
+            'absolute top-1/2 right-0 -translate-y-1/2 rotate-45 group transition-all duration-300',
             hover ? 'scale-125 translate-x-[71px]' : 'translate-x-1/2'
         )
     }, [hover])
@@ -60,44 +56,36 @@ const BasketPageItem = ({product, count, color = undefined, size = undefined}: p
 
 
     return (
-        <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}
-             className={containerClassName}>
-            <div className={wrapperClassName}>
-                <div className='w-2/5 flex items-center justify-between'>
-                    <Link href={`/Products/${product.id}`}>
-                        <a className='w-fit'><p className='link inline-block text-xl'>{product.name}</p></a>
-                    </Link>
-                    <div className='flex items-center gap-4'>
-                        {color && <PaletteItem color={color}/>}
-                        {size && <SizeItem size={size}/>}
+            <ItemRow backgroundContent={Colors._secondary_light} backgroundContentOnHover={Colors._black}
+                     toggleHover={setHover} heightOnPx={96}>
+                <div className={wrapperClassName}>
+                    <div className='w-2/5 flex items-center justify-between'>
+                        <Link href={`/Products/${product.id}`}>
+                            <a className='w-fit'><p className='link inline-block text-xl'>{product.name}</p></a>
+                        </Link>
+                        <div className='flex items-center gap-4'>
+                            {color && <PaletteItem color={color}/>}
+                            {size && <SizeItem size={size}/>}
+                        </div>
+                    </div>
+                    <div className='w-2/5 flex items-center justify-between '>
+                        <MultiButton id={product.id} reduceCount={handleReduce}
+                                     increaseCount={handleIncrease}/>
+                        <span
+                            className='text-weef-white'><span>{product.final_price}</span><span
+                            className='px-2'>تومان</span></span>
                     </div>
                 </div>
-                <div className='w-2/5 flex items-center justify-between '>
-                    <MultiButton id={product.id} reduceCount={handleReduce}
-                                 increaseCount={handleIncrease}/>
-                    <span
-                        className='text-weef-white'><span>{product.final_price}</span><span
-                        className='px-2'>تومان</span></span>
-                </div>
-            </div>
-            <button onClick={handleRemoveAll}
-                    title='حذف این محصول'
-                    className='absolute top-0 left-0 -translate-x-1/2 translate-y-[calc(100%-54px)] w-[68px] h-[68px] rotate-45 group bg-primary-red p-[1px] flex items-center justify-center'>
-                <span
-                    className='w-full h-full bg-weef-black group-hover:bg-transparent flex items-center justify-center'>
-                    <span className='-rotate-45 text-weef-white group-hover:text-weef-black'>حذف</span>
-                </span>
-            </button>
-            <div className={imageWrapper}>
-                <span
-                    className='w-full h-full bg-weef-black flex items-center justify-center overflow-hidden'>
-                    <span
-                        className='-rotate-45 text-weef-white group-hover:text-weef-black flex justify-center items-center overflow-visible w-full h-full scale-[1.4]'>
-                        <Image src={product.main_image} alt={product.name} objectFit={'cover'} layout={'fill'}/>
-                    </span>
-                </span>
-            </div>
-        </div>
+                <RotatedButton className='absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2' heightOnPx={96} title='حذف این محصول' onClick={handleRemoveAll} background={Colors._primary_red}>
+                    <div className='text-weef-white group-hover:text-weef-black'>حذف</div>
+                </RotatedButton>
+                <RotatedButton className={imageWrapper} heightOnPx={146} background={Colors._primary_orange}>
+                    <div className='flex justify-center items-center overflow-visible w-full h-full scale-[1.4]'>
+                    <Image className='select-none' src={product.main_image} alt={product.name}
+                           objectFit={'cover'} layout={'fill'}/>
+                    </div>
+                </RotatedButton>
+            </ItemRow>
     );
 }
 
