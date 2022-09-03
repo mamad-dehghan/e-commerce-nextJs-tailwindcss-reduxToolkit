f464622d88471e1abb4abc6f0581742c48cd4b7e
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 14:15:21 +0430

    feat(toast): move toast container and css imports to _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ import {PersistGate} from "redux-persist/integration/react";
 import {useRouter} from 'next/router';
 import {useMemo} from "react";
 import {CookiesProvider} from "react-cookie";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({Component, pageProps}: AppProps) {
     const router = useRouter()
@@ -23,6 +25,15 @@ function MyApp({Component, pageProps}: AppProps) {
             <Provider store={store}>
                 <PersistGate persistor={persist}>
                     <Component key={key} {...pageProps} />
+                    <ToastContainer theme="dark"
+                                    position="bottom-right"
+                                    hideProgressBar
+                                    newestOnTop
+                                    closeOnClick
+                                    rtl
+                                    pauseOnFocusLoss
+                                    draggable
+                                    pauseOnHover/>
                 </PersistGate>
             </Provider>
         </CookiesProvider>
