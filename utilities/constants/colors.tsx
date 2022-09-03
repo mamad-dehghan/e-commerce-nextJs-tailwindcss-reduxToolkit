dc6f6f4271cbbef9cb90e24c838d9a58bb0b8533
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 13:18:50 +0430

    feat(colors enum): add colors enum that refer to string value of each color

---
+++ utilities/constants/colors.tsx
@@ -0,0 +1,9 @@
+export enum Colors {
+    _primary_red = '#FF626D',
+    _primary_orange = '#FCAD72',
+    _black = '#232227',
+    _secondary = '#333235',
+    _white = '#FAFAFA',
+    _secondary_light = '#4C4C4C',
+    _grey = '#C2C2C2'
+}
