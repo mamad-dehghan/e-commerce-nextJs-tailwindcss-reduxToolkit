0f187cc336f5ec586b423e8b94ad404a12ccce91
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 13:44:00 +0430

    fix(style): refactor component and add sizing to component and error section

--- components/common/TextArea/index.tsx
+++ components/common/TextArea/index.tsx
@@ -1,7 +1,11 @@
 import React, {InputHTMLAttributes, useEffect, useMemo, useState} from 'react';
 import classNames from "classnames";
 
-const TextArea = ({className, about, ...props}: InputHTMLAttributes<HTMLTextAreaElement>) => {
+interface props extends InputHTMLAttributes<HTMLTextAreaElement> {
+    widthOnPercent: number
+}
+
+const TextArea = ({className, widthOnPercent, about, ...props}: props) => {
     const [errorMessage, setErrorMessage] = useState<string>('')
     useEffect(() => {
         if (about)
@@ -14,14 +18,14 @@ const TextArea = ({className, about, ...props}: InputHTMLAttributes<HTMLTextArea
 
     const errorClassName = useMemo(() => {
         return classNames(
-            'absolute bg-primary-red h-full rounded-r w-[calc(100%-220px)] sm:w-[calc(100%-300px)] right-0 -z-10 top-0 flex items-center pr-1 pl-3 transition-transform ease-in-out duration-300 whitespace-nowrap',
-            about ? ' ring-[1px] ring-primary-red' : '-translate-x-[100%]'
+            'absolute bg-primary-red h-full rounded-md left-0 -z-10 top-0 flex items-center pr-1 pl-3 transition-transform ease-in-out duration-300 whitespace-nowrap',
+            about ? ' ring-[1px] ring-primary-red' : ''
         )
     }, [about]);
 
     const inputClassName = useMemo(() => {
         return classNames(
-            'bg-secondary h-32 text-weef-white ring-[1px] placeholder-weef-grey outline-0 focus:outline-0 text-base rounded-md focus:border-0 border-0 focus:ring-primary-red block w-[240px] sm:w-[320px] p-2 z-20 resize-none',
+            'bg-secondary h-32 text-weef-white ring-[1px] placeholder-weef-grey outline-0 focus:outline-0 text-base rounded-md focus:border-0 border-0 focus:ring-primary-red block p-2 z-20 resize-none',
             className,
             about ? 'ring-primary-red' : 'ring-secondary'
         )
@@ -32,8 +36,15 @@ const TextArea = ({className, about, ...props}: InputHTMLAttributes<HTMLTextArea
             <textarea
                 dir='auto'
                 className={inputClassName}
+                style={{
+                    width: `${widthOnPercent}%`
+                }}
                 {...props}/>
-            <div className={errorClassName}>
+            <div className={errorClassName}
+                 style={{
+                     width: `${widthOnPercent}%`,
+                     transform: about ? `translateX(${(100 - widthOnPercent) / widthOnPercent * 100}%)` : ''
+                 }}>
                 <div>{errorMessage}</div>
             </div>
         </div>
