e11b47f80cdd3c6d3da6ea16a998a0496525f5e2
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 14:04:35 +0430

    feat(discount): add discount section

--- pages/Basket/index.tsx
+++ pages/Basket/index.tsx
@@ -7,11 +7,14 @@ import BasketSum from "../../components/costum/BasketSum";
 import {singleProductType} from "../../redux/slices/BasketSlice";
 import Head from 'next/head'
 import ItemRow from "../../components/costum/ItemRow";
-import 'react-toastify/dist/ReactToastify.css';
+import {RootState} from "../../redux/store";
+import {Colors} from "../../utilities/constants/colors";
+import BasketDiscount from "../../components/costum/BasketDiscount";
 
 
 const Basket = () => {
-    const {products} = useSelector((state: any) => state.BasketSlice);
+    const {products} = useSelector((state: RootState) => state.BasketSlice);
+
     return (
         <DefaultLayout>
             <div className='w-full flex flex-col'>
@@ -23,29 +26,28 @@ const Basket = () => {
                     <div className='text-weef-white text-4xl font-medium py-10 px-10'>سبد خرید</div>
                     {
                         products.length === 0 ?
-                            (
-                                <div className='w-full flex flex-col items-center gap-2 py-10 px-16'>
-                                    <ItemRow backgroundContent='#333235' backgroundContentOnHover='#232227'>
-                                        <div
-                                            className='text-weef-white w-full text-center bg-secondary group-hover:bg-weef-black h-full flex items-center justify-center transition-all duration-300'>
-                                            سبد خرید شما خالی است
-                                        </div>
-                                    </ItemRow>
-                                </div>
-                            ) :
-                            (
-                                <div className='w-full flex flex-col items-center gap-2 py-10 pl-12 pr-28'>
-                                    {
-                                        products.map((item: singleProductType) => (
-                                                <BasketPageItem product={item.product} count={item.count}
-                                                                key={item.product.id} size={item.attribute.size}
-                                                                color={item.attribute.color}/>
-                                            )
+                            <div className='w-full flex flex-col items-center gap-2 py-10 px-16'>
+                                <ItemRow backgroundContent={Colors._secondary}
+                                         backgroundContentOnHover={Colors._black}>
+                                    <div
+                                        className='text-weef-white w-full text-center h-full flex items-center justify-center transition-all duration-300'>
+                                        سبد خرید شما خالی است
+                                    </div>
+                                </ItemRow>
+                            </div>
+                            :
+                            <div className='w-full flex flex-col items-center gap-2 py-10 pl-20 pr-32'>
+                                {
+                                    products.map((item: singleProductType) => (
+                                            <BasketPageItem product={item.product} count={item.count}
+                                                            key={item.product.id} size={item.attribute.size}
+                                                            color={item.attribute.color}/>
                                         )
-                                    }
-                                </div>
-                            )
+                                    )
+                                }
+                            </div>
                     }
+                    <BasketDiscount/>
                     <div className='flex items-center justify-center w-full py-20'>
                         <BasketSum/>
                     </div>
