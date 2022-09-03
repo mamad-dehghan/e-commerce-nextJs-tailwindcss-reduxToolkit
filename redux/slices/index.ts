bfd83b0b39337a3aec27ca38f7207e3f3ed73880
Mamad <dehghanseyedmohammad@gmail.com>
2022-09-03 13:31:54 +0430

    remove(authentication): remove authentication slice and move token to cookies

--- redux/slices/index.ts
+++ redux/slices/index.ts
@@ -2,15 +2,13 @@ import BasketSlice from "./BasketSlice";
 import {persistReducer} from 'redux-persist'
 import storage from "redux-persist/lib/storage";
 import {combineReducers} from "@reduxjs/toolkit";
-import AuthenticationSlice from "./AuthenticationSlice";
 import OrderSlice from "./OrderSlice";
 
 
 const combinedReducers = combineReducers({
     BasketSlice,
-    AuthenticationSlice,
     OrderSlice
 })
 
-const reducer = persistReducer({key: 'rootPersist', storage, whitelist: ['BasketSlice', 'AuthenticationSlice']}, combinedReducers)
+const reducer = persistReducer({key: 'rootPersist', storage, whitelist: ['BasketSlice']}, combinedReducers)
 export default reducer
